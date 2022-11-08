913c2d010b266bf33c103bde5a3db2f526312e88
Wemerson Nino <wemersonnino@gmail.com>
2022-11-08 14:30:40 -0300

    Create and session rec click in Local Storage and Session Storage.

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,17 +1,33 @@
 import React from "react";
 
-const Counter = () =>{
+const Counter = () => {
     const [count, setCount] = React.useState(0);
+    let test1;
+
+    React.useEffect(() => {
+        let test = count.toString();
+        localStorage.setItem('count', test);
+    }, [count]);
+
+    React.useEffect(() => {
+        (localStorage.getItem('count')) ? setCount(parseInt(localStorage.getItem('count'))) : sessionStorage.getItem('count');
+    }, [count]);
+
+    if (count !== 0){
+        test1 = count;
+    } else {
+        test1 = sessionStorage.getItem('count');
+    }
 
     function handleClick(event) {
         event.preventDefault();
 
-        setCount((previousValue)=>{
+        setCount((previousValue) => {
             return previousValue + 1;
         });
     }
 
-    return <a href="#" onClick={handleClick}>Counter: {count}</a>
+    return <a href={'/'} onClick={handleClick}>Counter: {test1}</a>
 }
 
 export default Counter;
